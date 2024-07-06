b5da3ef008babe75da0d75105f771a6ba22cf539
ikumi ojiro <ikumidudecool@gmail.com>
2024-07-06 13:37:59 +0900

    watchlist

---
+++ app/models/bookmark.rb
@@ -0,0 +1,6 @@
+class Bookmark < ApplicationRecord
+  belongs_to :list
+  belongs_to :movie
+  validates :movie_id, uniqueness: {scope: :list_id, message: "Must be unique per list"}
+  validates :comment, length: {minimum: 6}
+end
