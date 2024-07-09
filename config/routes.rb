9b658d92626546f557d5edb37655719695c34db4
ikumi ojiro <ikumidudecool@gmail.com>
2024-07-09 20:45:43 +0900

    fix

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,5 @@ Rails.application.routes.draw do
     resources :bookmarks, only: [:new, :create, :destroy]
   end
   resources :bookmarks, only: [:destroy]
+  root to: 'lists#index'
 end
-
-root to: 'lists#index'
